b084737c4fbcb7cd9f31cc4f35b99a9dc8427cb6
Nikola Stoyanov <ns.barzakov@gmail.com>
2018-12-22 12:49:23 +0200

    Split to lib and bin. Add a test

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use amethyst::assets::{PrefabLoader, RonFormat};
+use amethyst::core::timing::Time;
+use amethyst::prelude::*;
+use amethyst::utils::fps_counter::{FPSCounter};
+
+pub mod camera;
+pub mod controls;
+pub mod enemy;
+pub mod ground;
+pub mod movement;
+pub mod prefab;
+pub mod spawn;
+
+use self::prefab::GamePrefab;
+
+#[derive(Default, Debug)]
+pub struct GameState;
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, mut data: StateData<GameData>) {
+        let handle = data.world.exec(|loader: PrefabLoader<GamePrefab>| {
+            loader.load("prefab/scene.ron", RonFormat, (), ())
+        });
+
+        data.world.create_entity().with(handle).build();
+
+        ground::generate(&mut data.world, 30, 30);
+    }
+
+    fn fixed_update(&mut self, data: StateData<GameData>) -> Trans<GameData<'static, 'static>, StateEvent> {
+        let time = data.world.read_resource::<Time>();
+        let fps = data.world.read_resource::<FPSCounter>();
+
+        if time.frame_number() % 100 == 0 {
+            println!("fps: {}", fps.sampled_fps());
+        }
+
+        Trans::None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,16 @@
-use amethyst::assets::{PrefabLoader, PrefabLoaderSystem, RonFormat};
-use amethyst::core::timing::Time;
+use amethyst::assets::PrefabLoaderSystem;
 use amethyst::core::TransformBundle;
 use amethyst::prelude::*;
 use amethyst::renderer::{DrawShaded, PosNormTex};
-use amethyst::utils::fps_counter::{FPSCounter, FPSCounterBundle};
+use amethyst::utils::fps_counter::FPSCounterBundle;
 
-pub mod camera;
-pub mod controls;
-pub mod enemy;
-pub mod ground;
-pub mod movement;
-pub mod prefab;
-pub mod spawn;
-
-use self::camera::CameraSystem;
-use self::controls::InputBundle;
-use self::enemy::EnemySystem;
-use self::movement::MovementSystem;
-use self::prefab::GamePrefab;
-use self::spawn::SpawnSystem;
-
-#[derive(Default, Debug)]
-struct GameState;
-
-impl<'a, 'b> SimpleState<'a, 'b> for GameState {
-    fn on_start(&mut self, mut data: StateData<GameData>) {
-        let handle = data.world.exec(|loader: PrefabLoader<GamePrefab>| {
-            loader.load("prefab/scene.ron", RonFormat, (), ())
-        });
-
-        data.world.create_entity().with(handle).build();
-
-        ground::generate(&mut data.world, 30, 30);
-    }
-
-    fn fixed_update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>, StateEvent> {
-        let time = data.world.read_resource::<Time>();
-        let fps = data.world.read_resource::<FPSCounter>();
-
-        if time.frame_number() % 100 == 0 {
-            println!("fps: {}", fps.sampled_fps());
-        }
-
-        Trans::None
-    }
-}
+use simple_towers::GameState;
+use simple_towers::camera::CameraSystem;
+use simple_towers::controls::InputBundle;
+use simple_towers::enemy::EnemySystem;
+use simple_towers::movement::MovementSystem;
+use simple_towers::prefab::GamePrefab;
+use simple_towers::spawn::SpawnSystem;
 
 fn start_logger() {
     use amethyst::{LogLevelFilter, LoggerConfig, StdoutLog};
